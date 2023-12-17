28d3c5dc177a2f0f4ebe5dbd78636a2cfd95d996
Milos Tomic <milos.tomic@sap.com>
2023-12-17 11:30:02 +0100

    ip range and stuff

--- pkg/common/actions/scope/loadKyma.go
+++ pkg/common/actions/scope/loadKyma.go
@@ -9,6 +9,7 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 )
 
 func loadKyma(ctx context.Context, st composed.State) (error, context.Context) {
@@ -16,13 +17,16 @@ func loadKyma(ctx context.Context, st composed.State) (error, context.Context) {
 	state := st.(*State)
 
 	kymaUnstructured := util.NewKymaUnstructured()
-	err := state.Client().Get(ctx, state.Name(), kymaUnstructured)
+	err := state.Client().Get(ctx, types.NamespacedName{
+		Name:      state.CommonObj().KymaName(),
+		Namespace: state.Obj().GetNamespace(),
+	}, kymaUnstructured)
 	if apierrors.IsNotFound(err) {
 		meta.SetStatusCondition(state.CommonObj().Conditions(), metav1.Condition{
 			Type:    cloudresourcesv1beta1.ConditionTypeError,
 			Status:  metav1.ConditionTrue,
 			Reason:  cloudresourcesv1beta1.ReasonInvalidKymaName,
-			Message: fmt.Sprintf("The Kyma CR '%s' does not exit", state.Name()),
+			Message: fmt.Sprintf("The Kyma CR '%s' does not exit", state.CommonObj().KymaName()),
 		})
 		err = state.UpdateObjStatus(ctx)
 		if err != nil {
@@ -31,7 +35,7 @@ func loadKyma(ctx context.Context, st composed.State) (error, context.Context) {
 			return composed.StopWithRequeue, nil // requeue so object status is updated to invalid kyma
 		}
 
-		return composed.StopAndForget, nil // done, no requeue
+		return composed.StopAndForget, nil // status is set, no requeue since it refers to missing kyma cr
 	}
 
 	if err != nil {
