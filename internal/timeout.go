da18b659a342ec35667703f06efb34e2a2173d41
Dušan Pantić <dushanpantic@users.noreply.github.com>
2024-08-30 15:12:40 +0200

    chore: adjust timeout (#25)

--- internal/timeout.go
+++ internal/timeout.go
@@ -1,12 +1,13 @@
 package internal
 
 import (
-	"github.com/onsi/gomega"
 	"time"
+
+	"github.com/onsi/gomega"
 )
 
 const (
-	DefaultEventuallyTimeout           = 5 * time.Minute
+	DefaultEventuallyTimeout           = 10 * time.Minute
 	DefaultEventuallyPollingInterval   = 10 * time.Second
 	DefaultConsistentlyDuration        = 20 * time.Second
 	DefaultConsistentlyPollingInterval = 5 * time.Second
